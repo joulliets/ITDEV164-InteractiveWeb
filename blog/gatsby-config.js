b3e74f276cad7cec414a744a0cc9ad5aa592dfbd
Jouliet Morales <jouliet.25@gmai.com>
2021-02-09 22:26:21 -0600

    Completed Activity 2

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,39 +1,39 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Blog`,
-    description: `ITDEV-164 Gatsby Blog.`,
-    author: `Jouliet Morales`,
+    title: 'Gatsby Blog',
+    description: 'ITDEV-164 Gatsby Blog.',
+    author: 'Jouliet Morales',
     contact: {
-      name: `Jouliet Morales`,
-      company: `Blogs Inc.`,
-      address: `PO Box 1234`
-    }
+      name: 'Jouliet Morales',
+      company: 'Blogs Inc.',
+      address: 'PO Box 1234',
+    },
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-contentful',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        spaceId: 'ctticsfutc3a',
+        accessToken: 'Xmyk_gnYk53AAQuAs0UzrjC168BxqHw8l3Dmii3R7pE',
+      }
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    'gatsby-plugin-offline'
   ],
 }
