41772084a894c5fe12d253ff12f6b89946ed3bbc
Jouliet Morales <jouliet.25@gmai.com>
2021-02-13 14:39:57 -0600

    Completed Activity 3

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,39 +1,48 @@
+// Initialize dotenv
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Blog',
-    description: 'ITDEV-164 Gatsby Blog.',
-    author: 'Jouliet Morales',
+    title: "Gatsby Blog",
+    description: "ITDEV-164 Gatsby Blog.",
+    author: "Jouliet Morales",
     contact: {
-      name: 'Jouliet Morales',
-      company: 'Blogs Inc.',
-      address: 'PO Box 1234',
+      name: "Jouliet Morales",
+      company: "Blogs Inc.",
+      address: "PO Box 1234",
     },
   },
   plugins: [
     {
-      resolve: 'gatsby-source-contentful',
+      resolve: "gatsby-source-contentful",
       options: {
-        spaceId: 'ctticsfutc3a',
-        accessToken: 'Xmyk_gnYk53AAQuAs0UzrjC168BxqHw8l3Dmii3R7pE',
-      }
+        spaceId: `${process.env.SPACE_ID}`,
+        accessToken: `${process.env.ACCESS_TOKEN}`,
+      },
     },
-    'gatsby-plugin-react-helmet',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+
+    "gatsby-transformer-remark",
+    "gatsby-plugin-react-helmet",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-image",
+
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        name: "gatsby-starter-default",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "src/images/gatsby-icon.png",
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    'gatsby-plugin-offline'
+    "gatsby-plugin-offline",
   ],
 }
--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,5 +1,6 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { graphql } from "gatsby"
+import { Link } from 'gatsby'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -10,14 +11,20 @@ const IndexPage = ({ data }) => (
     <ul>
       {data.allContentfulBlogPost.edges.map(edge => (
         <li>
-          <Link to={edge.node.slug} key={edge.node.id}>{edge.node.title}</Link>
+          <Link to={edge.node.slug} key={edge.node.id}>
+            {edge.node.title}
+          </Link>
+          {edge.node.heroImage && (
+          <div>
+            <img src={edge.node.heroImage.fluid.src} alt="hero pic" />
+          </div>
+          )}
+          <div>{edge.node.body.childMarkdownRemark.excerpt}</div>
         </li>
-      ))
-    }
+      ))}
     </ul>
   </Layout>
 )
-
 export default IndexPage
 
 export const query = graphql`
@@ -28,6 +35,16 @@ export const query = graphql`
           id
           title
           slug
+          heroImage {
+            fluid(maxWidth: 300) {
+              src
+            }
+          }
+          body {
+            childMarkdownRemark {
+              excerpt
+            }
+          }
         }
       }
     }
